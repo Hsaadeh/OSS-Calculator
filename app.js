1f9919ecc6303b0514d3eb272aa5f3e7f97ea6e5
Hsaadeh <hsaadeh00@gmail.com>
2021-10-20 17:22:01 -0500

    refactored js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import {MDCRipple} from '@material/ripple';
 
 
 let calculateButton = document.querySelector("#calculateButton");
@@ -11,45 +10,52 @@ let LFDCPP = 1.75;
 let BookCPP = 0.25
 let MagCPP = 0.25;
 
-// Is there a better way to initialize the values then clear them with the clear button below?
-document.querySelector("#FD33value").value = 0;
-document.querySelector("#FD24value").value = 0;
-document.querySelector("#DABvalue").value = 0;
-document.querySelector("#LDABvalue").value = 0;
-document.querySelector("#OS36value").value = 0;
-document.querySelector("#LFDvalue").value = 0;
-document.querySelector("#B24value").value = 0;
-document.querySelector("#MAGvalue").value = 0;
+// Initialized values
+let FD33value = document.querySelector("#FD33value").value = 0;
+let FD24value = document.querySelector("#FD24value").value = 0;
+let DABvalue = document.querySelector("#DABvalue").value = 0;
+let LDABvalue = document.querySelector("#LDABvalue").value = 0;
+let OS36value = document.querySelector("#OS36value").value = 0;
+let LFDvalue = document.querySelector("#LFDvalue").value = 0;
+let B24value = document.querySelector("#B24value").value = 0;
+let MAGvalue = document.querySelector("#MAGvalue").value = 0;
 
+let standardTypeCost = ((FD33value + FD24value + DABvalue + LDABvalue + OS36value) * standardCPP);
+let specialTypeCost = ((LFDvalue * LFDCPP) + (B24value * BookCPP) + (MAGvalue * MagCPP));
 
+let TotalCost = (standardTypeCost + specialTypeCost);
+
+// Calculate Button
 calculateButton.addEventListener("click", () => {
     // standard types ---> variable = Number(domElement.value * (number of pages))
-    let FD33value = Number(document.querySelector("#FD33value").value * (5800));
-    let FD24value = Number(document.querySelector("#FD24value").value * (4200));
-    let DABvalue = Number(document.querySelector("#DABvalue").value * (2700));
-    let LDABvalue = Number(document.querySelector("#LDABvalue").value * (4200));
-    let OS36value = Number(document.querySelector("#OS36value").value * (6300));
+    FD33value = Number(document.querySelector("#FD33value").value * (5800));
+    FD24value = Number(document.querySelector("#FD24value").value * (4200));
+    DABvalue = Number(document.querySelector("#DABvalue").value * (2700));
+    LDABvalue = Number(document.querySelector("#LDABvalue").value * (4200));
+    OS36value = Number(document.querySelector("#OS36value").value * (6300));
 
     // special types
-    let LFDvalue = Number(document.querySelector("#LFDvalue").value * (1));
-    let B24value = Number(document.querySelector("#B24value").value * (300));
-    let MAGvalue = Number(document.querySelector("#MAGvalue").value * (80));
+    LFDvalue = Number(document.querySelector("#LFDvalue").value * (1));
+    B24value = Number(document.querySelector("#B24value").value * (300));
+    MAGvalue = Number(document.querySelector("#MAGvalue").value * (80));
 
-    let standardTypeCost = ((FD33value + FD24value + DABvalue + LDABvalue + OS36value) * standardCPP);
-    let specialTypeCost = ((LFDvalue * LFDCPP) + (B24value * BookCPP) + (MAGvalue * MagCPP))
+    standardTypeCost = ((FD33value + FD24value + DABvalue + LDABvalue + OS36value) * standardCPP);
+    specialTypeCost = ((LFDvalue * LFDCPP) + (B24value * BookCPP) + (MAGvalue * MagCPP));
 
-    let TotalCost = (standardTypeCost + specialTypeCost)
-    
+    TotalCost = (standardTypeCost + specialTypeCost);
+
     console.log(TotalCost);
 });
 
+// Clear Button
 clearButton.addEventListener("click", () => {
-    document.querySelector("#FD33value").value = 0;
-    document.querySelector("#FD24value").value = 0;
-    document.querySelector("#DABvalue").value = 0;
-    document.querySelector("#LDABvalue").value = 0;
-    document.querySelector("#OS36value").value = 0;
-    document.querySelector("#LFDvalue").value = 0;
-    document.querySelector("#B24value").value = 0;
-    document.querySelector("#MAGvalue").value = 0;
-})
+    FD33value = document.querySelector("#FD33value").value = 0;
+    FD24value = document.querySelector("#FD24value").value = 0;
+    DABvalue = document.querySelector("#DABvalue").value = 0;
+    LDABvalue = document.querySelector("#LDABvalue").value = 0;
+    OS36value = document.querySelector("#OS36value").value = 0;
+    LFDvalue = document.querySelector("#LFDvalue").value = 0;
+    B24value = document.querySelector("#B24value").value = 0;
+    MAGvalue = document.querySelector("#MAGvalue").value = 0;
+});
+
